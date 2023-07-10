51b437f37174a5b268a03fc5975da7661f5f4db0
KoblizekXD <janprokupek@centrum.cz>
2023-07-10 16:38:25 +0200

    added: stuff to ClassMapping

--- src/main/java/io/github/koblizekxd/qmapper/mappings/ClassMapping.java
+++ src/main/java/io/github/koblizekxd/qmapper/mappings/ClassMapping.java
@@ -1,4 +1,19 @@
 package io.github.koblizekxd.qmapper.mappings;
 
 public class ClassMapping {
+    private final String old;
+    private final String name;
+
+    public ClassMapping(String old, String name) {
+        this.old = old;
+        this.name = name;
+    }
+
+    public String getOldName() {
+        return old;
+    }
+
+    public String getNewName() {
+        return name;
+    }
 }
